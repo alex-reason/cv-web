c077861a1f6ca8502944c025fa1f949894b77e3e
Alecs Reasonda <momai.reason@gmail.com>
2023-04-18 12:26:42 -0400

    fixed minor typo

--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -6,14 +6,16 @@ const Landing = () => {
             <div className='landing__item landing__description'>
                 <h1>Creativity, Curiosity, Consistency. </h1>
                 <p>
-                    I'm Alexandra Mai!
+                    I'm Alexandra!
                     Looking for front-end web development or non-profit work. I welcome the opportunity to use my React and website development skills.
                 </p>
                 <a className='button' href='#footer'>Reach out</a>
             </div>
 
             <div className='landing__item landing__image'>
-                <img src={mirror} alt='landing-flower' />
+                <img src={mirror} 
+                title='doodle by me'
+                alt='landing-flower' />
                 <div className='shadow'></div>
             </div>
         </heading>
